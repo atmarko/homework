05452a0285fc999f032d05b924de80d3ef375dc2
Marko Mihajlov <119135923+atmarko@users.noreply.github.com>
2023-03-07 00:31:02 +0100

    Updated shellscript-3.sh
    changed echo with printf.
    Echo did not print \n in new row when executed with bash but it did when executed with sh then script name.
    Curious.

--- Homework 6 - Bash Scripts/shellscript-3.sh
+++ Homework 6 - Bash Scripts/shellscript-3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Write a shell script that displays "plan code build test release deploy" on the screen with each appearing on a separate line.
 
-echo "plan\ncode\nbuild\ntest\nrelease\ndeploy"
+printf "plan\ncode\nbuild\ntest\nrelease\ndeploy"
